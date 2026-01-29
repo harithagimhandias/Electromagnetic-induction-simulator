1b273a9470256c73ee31ad741524e32f455df63c
harithagimhandias <harithadias3@gmail.com>
2026-01-29 09:31:18 +0530

    Create lenz_law_intro.py

---
+++ lenz_law_intro.py
@@ -0,0 +1,8 @@
+"""
+Electromagnetic Induction Simulator
+Topic: Lenz's Law
+
+This file introduces the physical idea behind Lenz's law
+"""
+print("Lenz's law states that the induced current opposes the change that causes it")
+print("This simulator helps visualize the process of induced current and undestand how lenz's law work")
