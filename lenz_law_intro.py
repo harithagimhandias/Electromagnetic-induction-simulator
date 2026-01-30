b11c8b5a5ba0a86e318b460698b45f00c6105766
harithagimhandias <harithadias3@gmail.com>
2026-01-30 13:31:32 +0530

    Update lenz_law_intro.py

--- lenz_law_intro.py
+++ lenz_law_intro.py
@@ -6,3 +6,17 @@ This file introduces the physical idea behind Lenz's law
 """
 print("Lenz's law states that the induced current opposes the change that causes it")
 print("This simulator helps visualize the process of induced current and undestand how lenz's law work")
+
+# Simple Lenz's law demonstration
+
+magnet_direction = input("Is the magnet oving towards or away from the coil?")
+
+if magnet_direction == "towards":
+  current_direction = "anti-clockwise"
+elif magnet_direction == "away":
+  current_direction = "clockwise"
+else:
+  current_direction = "unknown"
+
+print("The induced current is:", current_direction)
+
